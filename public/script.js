04846dbc0777b1390044871c4247de83baeef9e4
thesongmartins <martinsjehoshaphat@gmail.com>
2024-10-31 17:46:58 +0100

    updated

---
+++ public/script.js
@@ -0,0 +1,12 @@
+const menu = document.querySelector('menu');
+
+const open = function () {
+    menu.classList.remove('hidden');
+    console.log('Button clicked');
+}
+
+const closeMenu = function () {
+    menu.classList.add('hidden');
+}
+menu.addEventListener('click', open);
+menu.addEventListener('click', closeMenu);
