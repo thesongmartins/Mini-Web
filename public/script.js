c9e6f8b3b0c5fcc7f58e8e9c48f36d0efba0ccfa
thesongmartins <martinsjehoshaphat@gmail.com>
2024-11-16 16:26:22 +0100

    Updated Code

--- public/script.js
+++ public/script.js
@@ -1,15 +1,22 @@
-const { list } = require("postcss");
+// const { list } = require("postcss");
 
 let nav = document.querySelector('.nav');
 let btn = document.querySelector('.toggle');
-let icon = btn.querySelector('.fa-bars');
-
+let icon = btn.querySelector('.icon');
+console.log(btn, nav, icon);
 btn.addEventListener('click', () => {
-    if (icon.classList.contains('fa')) {
-        nav.classList.remove('hidden');
+    // if (icon.classList.contains('fa')) {
+    //     nav.classList.remove('hidden');
+    //     icon.classList.replace('fa-bars', 'fa-times');
+    // } else {
+    //     icon.classList.replace('fa-times', 'fa-bars');
+    //     nav.classList.add('hidden');
+    // }
+    nav.classList.toggle('hidden');
+    // icon.classList.replace('fa-bars', 'fa-times');
+    if (icon.classList.contains('fa-bars')) {
         icon.classList.replace('fa-bars', 'fa-times');
     } else {
         icon.classList.replace('fa-times', 'fa-bars');
-        nav.classList.add('hidden');
     }
 })
